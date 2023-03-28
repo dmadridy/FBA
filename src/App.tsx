d0e5337a814d3ef78ce439aa5fd7c500ec44f7c2
David Madrid <davidmadridpathway@gmail.com>
2023-03-28 16:54:25 -0500

    Setting up the Routering

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import {
+  createBrowserRouter,
+  createRoutesFromChildren,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import Home from './components/home';
+import Root from './layout/root';
+
 const App = () => {
-  return <div>App</div>;
+  const router = createBrowserRouter(
+    createRoutesFromChildren(
+      <Route path='/' element={<Root />}>
+        <Route index element={<Home />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
 };
 
 export default App;
