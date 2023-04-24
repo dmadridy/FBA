e60b49d263a3599b7b83d05dc672e9dfc75f583d
David Madrid <davidmadridpathway@gmail.com>
2023-04-24 16:52:11 -0500

    Projects component responsive finished

--- src/assets/styles/index.tsx
+++ src/assets/styles/index.tsx
@@ -1,6 +1,6 @@
 export const activeStyle = 'text-indigo-300 duration-200 font-medium';
 export const unactiveStyle = 'text-gray-300 duration-200 hover:text-indigo-300';
 export const projectsBoxStyle =
-  'p-8 rounded-lg hover:bg-gray-800 duration-300 hover:last:text-indigo-400 border border-gray-800';
+  'p-8 rounded-lg hover:bg-gray-800 duration-300 hover:last:text-indigo-400 text-indigo-400 md:text-white border border-gray-800';
 export const activeSidebar = 'font-medium text-indigo-300';
 export const unactiveSidebar = 'font-medium';
